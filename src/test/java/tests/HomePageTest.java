8035f51a9a38dcbbb6189e58b8e2d136ba964791
AlphaDelta <35943388+AlphaDelta2469@users.noreply.github.com>
2018-01-29 21:04:27 -0500

    Initial commit

---
+++ src/test/java/tests/HomePageTest.java
@@ -0,0 +1,29 @@
+package tests;
+
+import static org.testng.Assert.*;
+import org.testng.annotations.Test;
+
+public class HomePageTest {
+    
+    @Test
+    public void testExample1() {
+        
+        assertEquals("true", "true");
+   
+    }
+    
+    @Test
+    public void testExample2() {
+        
+        assertEquals("true", "true");
+   
+    }
+    
+    @Test
+    public void testExample3() {
+        
+        assertEquals("true", "false");
+   
+    }
+     
+}
---
+++ src/test/java/tests/SignInPageTest.java
@@ -0,0 +1,29 @@
+package tests;
+
+import static org.testng.Assert.*;
+import org.testng.annotations.Test;
+
+public class SignInPageTest {
+    
+    @Test
+    public void testExampleA() {
+        
+        assertEquals("true", "true");
+   
+    }
+    
+    @Test
+    public void testExampleB() {
+        
+        assertEquals("true", "true");
+   
+    }
+    
+    @Test
+    public void testExampleC() {
+        
+        assertEquals("true", "false");
+   
+    }
+     
+}
